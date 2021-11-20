496e22ee18c5c7c2f5d32f93f784f6b59237d0b8
Antoni Vinicius <72105624+antonivini1@users.noreply.github.com>
2021-11-19 21:23:43 -0300

    Update Projeto.py

--- Projeto.py
+++ Projeto.py
@@ -2,12 +2,26 @@ from PIL import Image
 import numpy as np
 import math
 
+def split_channels(img):
+    Red = np.asarray(img)
+    Red[:,:,1] *= 0
+    Red[:,:,2] *= 0
+
+    Green = np.asarray(img)
+    Green[:,:,0] *= 0
+    Green[:,:,2] *= 0
+
+    Blue = np.asarray(img)
+    Blue[:,:,0] *= 0
+    Blue[:,:,1] *= 0
+    
+    return Image.fromarray(Red), Image.fromarray(Green), Image.fromarray(Blue)
 def which_c(number):
     if(number == 0):
         return math.sqrt(0.5)
     else:
         return 1
-def dct(x,N):
+def DCT(x,N):
     X = np.zeros(N)
     for k in range(N):
         c = which_c(k)
@@ -16,31 +30,41 @@ def dct(x,N):
             summ += x[n] * math.cos(((2*n+1)*math.pi*k)/(2*N))
         X[k] = math.sqrt(2/N) * c * summ
     return X
+def iDCT(X,N):
+    x = np.zeros(N)
+    for n in range(N):
+        summ = 0
+        for k in range(N):
+            c = which_c(k)
+            summ += X[k] * c * math.cos(((2*n+1)*math.pi*k)/(2*N))
+        x[n] = math.sqrt(2/N) * summ
+    return x
 
 img_src = Image.open("lena256.png")
 img_arr = np.asarray(img_src)
 R,C = img_arr.shape
 X = np.zeros((R,C))
-#img_dct = np.zeros((R,C))
-
-# for k in range(R):
-#     c_k = which_c(k)
-#     for l in range(C):
-#         c_l = which_c(l)
-#         summ = 0
-#         for m in range(R):
-#             for n in range(C):
-#                 summ == img_arr[m,n] * math.cos(((2*m + 1)*k*math.pi)/(2*R)) * math.cos(((2*n + 1)*l*math.pi)/(2*C))
-#         X[k,l] = (2/math.sqrt(R*C)) * c_k * c_l * summ
+x = np.zeros((R,C))
+img_src.show()
 
 for k in range(R):
-    X[k,:] = dct(img_arr[k,:],R)
+    X[k,:] = DCT(img_arr[k,:],R)
 for l in range(C):
-    X[:,l] = dct(X[:,l],C)
-
+    X[:,l] = DCT(X[:,l],C)
 #X /= X[0][0]
-img_dct = np.log(np.abs(X)+1)
-img_dct *= (255.0/img_dct.max())
+
+#img_dct = np.log(np.abs(X)+1)
+img_dct = X * (255.0/X.max())
 img_dct = Image.fromarray(img_dct)
 img_dct = img_dct.convert("P")
 img_dct.show()
+
+for m in range(R):
+    x[m,:] = iDCT(X[m,:],R)
+for n in range(C):
+    x[:,n] = iDCT(x[:,n],C)
+
+img_idct = x * (255.0/x.max())
+img_idct = Image.fromarray(img_idct)
+img_idct = img_idct.convert("P")
+img_idct.show()
